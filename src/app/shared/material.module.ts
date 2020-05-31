b52bf302dc4302a51944d50f4beae0492438caef
rsv-k <rsv.kpast@gmail.com>
2020-05-31 16:28:55 +0300

    word component display

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatCardModule } from '@angular/material/card';
 
 
 
 @NgModule({
    exports: [
-      MatExpansionModule
+      MatExpansionModule,
+      MatCardModule
    ]
 })
 export class MaterialModule { }
