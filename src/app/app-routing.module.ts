c992ed29bec21e06ebe242f28a5e539d5d45e504
rsv-k <rsv.kpast@gmail.com>
2020-05-31 15:59:57 +0300

    dictionary feature basic setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+   { path: '', redirectTo: '/dictionary', pathMatch: 'full' },
+   { path: 'dictionary', loadChildren: () => import('@features/dictionary/dictionary.module').then(m => m.DictionaryModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/features/dictionary/dictionary-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DictionaryComponent } from './pages/dictionary/dictionary.component';
+
+
+const routes: Routes = [
+   { path: '', component: DictionaryComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class DictionaryRoutingModule { }
