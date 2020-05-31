d45b163f6bc0314f3e0445a892f111aac65d5f34
rsv-k <rsv.kpast@gmail.com>
2020-05-31 16:29:59 +0300

    word-create component basic init

---
+++ src/app/features/dictionary/components/word-create/word-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-word-create',
+  templateUrl: './word-create.component.html',
+  styleUrls: ['./word-create.component.scss']
+})
+export class WordCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/features/dictionary/dictionary.module.ts
+++ src/app/features/dictionary/dictionary.module.ts
@@ -5,10 +5,11 @@ import { DictionaryRoutingModule } from './dictionary-routing.module';
 import { DictionaryComponent } from './pages/dictionary/dictionary.component';
 import { WordComponent } from './components/word/word.component';
 import { SharedModule } from '@shared/shared.module';
+import { WordCreateComponent } from './components/word-create/word-create.component';
 
 
 @NgModule({
-  declarations: [DictionaryComponent, WordComponent],
+  declarations: [DictionaryComponent, WordComponent, WordCreateComponent],
   imports: [
     CommonModule,
     DictionaryRoutingModule,
