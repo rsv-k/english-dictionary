28bd2bffba5088dba266d2b6946bad9831729af8
rsv-k <rsv.kpast@gmail.com>
2020-05-31 16:39:59 +0300

    word create component display

--- src/app/features/dictionary/components/word-create/word-create.component.ts
+++ src/app/features/dictionary/components/word-create/word-create.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-word-create',
-  templateUrl: './word-create.component.html',
-  styleUrls: ['./word-create.component.scss']
+   selector: 'app-word-create',
+   templateUrl: './word-create.component.html',
+   styleUrls: ['./word-create.component.scss']
 })
 export class WordCreateComponent implements OnInit {
 
-  constructor() { }
+   constructor() { }
 
-  ngOnInit(): void {
-  }
+   ngOnInit(): void {
+   }
 
 }
--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatCardModule } from '@angular/material/card';
-
-
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
    exports: [
       MatExpansionModule,
-      MatCardModule
+      MatCardModule,
+      MatInputModule,
+      MatButtonModule
    ]
 })
 export class MaterialModule { }
