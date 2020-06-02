55a2a3536ae4774ed44ba6b9f719159e70b6b81f
rsv-k <rsv.kpast@gmail.com>
2020-06-02 23:34:50 +0300

    set component basic init

--- client/src/app/features/sets/sets.module.ts
+++ client/src/app/features/sets/sets.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { SetsRoutingModule } from './sets-routing.module';
 import { SetsComponent } from './pages/sets/sets.component';
 import { SharedModule } from '@shared/shared.module';
+import { SetComponent } from './components/set/set.component';
 
 
 @NgModule({
-   declarations: [SetsComponent],
+   declarations: [SetsComponent, SetComponent],
    imports: [
       CommonModule,
       SetsRoutingModule,
