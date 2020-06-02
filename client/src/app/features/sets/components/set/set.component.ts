4870eff4280e8427ff0b8cd973a2ca9d8de4c39c
rsv-k <rsv.kpast@gmail.com>
2020-06-02 23:44:09 +0300

    set component basic structure

--- client/src/app/features/sets/components/set/set.component.ts
+++ client/src/app/features/sets/components/set/set.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Set } from '@core/models/set.model';
 
 @Component({
-  selector: 'app-set',
-  templateUrl: './set.component.html',
-  styleUrls: ['./set.component.scss']
+   selector: 'app-set',
+   templateUrl: './set.component.html',
+   styleUrls: ['./set.component.scss']
 })
 export class SetComponent implements OnInit {
+   @Input() set: Set;
 
-  constructor() { }
+   constructor() { }
 
-  ngOnInit(): void {
-  }
+   ngOnInit(): void {
+   }
 
 }
