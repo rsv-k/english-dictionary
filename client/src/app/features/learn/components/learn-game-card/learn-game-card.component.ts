5ff4637a8b1cbef2235c25e081d7cd3ffe2b48a9
rsv-k <rsv.kpast@gmail.com>
2020-06-07 11:00:51 +0300

    learn-game-card component basic init

---
+++ client/src/app/features/learn/components/learn-game-card/learn-game-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-learn-game-card',
+  templateUrl: './learn-game-card.component.html',
+  styleUrls: ['./learn-game-card.component.scss']
+})
+export class LearnGameCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- client/src/app/features/learn/learn.module.ts
+++ client/src/app/features/learn/learn.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LearnRoutingModule } from './learn-routing.module';
-import { LearnComponent } from './pages/learn/learn.component';
-import { WordTranslationWordComponent } from './components/word-translation-word/word-translation-word.component';
 import { SharedModule } from '@shared/shared.module';
-import { SavannahComponent } from './pages/savannah/savannah.component';
+
+import { LearnComponent } from './pages/learn/learn.component';
+import { LearnGameCardComponent } from './components/learn-game-card/learn-game-card.component';
 
 
 @NgModule({
-   declarations: [LearnComponent, WordTranslationWordComponent, SavannahComponent],
+   declarations: [LearnComponent, LearnGameCardComponent],
    imports: [
       CommonModule,
       LearnRoutingModule,
