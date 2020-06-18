ec7a40bd81c886e8cf2e933cedc3c26c55912c51
rsv-k <rsv.kpast@gmail.com>
2020-06-19 00:23:10 +0300

    app centered projection component added

---
+++ client/src/app/shared/components/centered-projection/centered-projection.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-centered-projection',
+  templateUrl: './centered-projection.component.html',
+  styleUrls: ['./centered-projection.component.scss']
+})
+export class CenteredProjectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -2,18 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from './material.module';
 import { CutLengthPipe } from './pipes/cut-length.pipe';
-
-
+import { CenteredProjectionComponent } from './components/centered-projection/centered-projection.component';
 
 @NgModule({
-   declarations: [CutLengthPipe],
-   imports: [
-      CommonModule,
-      MaterialModule
-   ],
-   exports: [
-      CutLengthPipe,
-      MaterialModule
-   ]
+   declarations: [CutLengthPipe, CenteredProjectionComponent],
+   imports: [CommonModule, MaterialModule],
+   exports: [CutLengthPipe, MaterialModule, CenteredProjectionComponent]
 })
-export class SharedModule { }
+export class SharedModule {}
