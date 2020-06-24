cf2e5622e5aeea66e1567ca9395754adbf9a198c
rsv-k <rsv.kpast@gmail.com>
2020-06-24 19:32:56 +0300

    http interceptor moved to core module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,8 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from '@core/core.module';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthInterceptor } from '@core/interceptors/auth.interceptor';
 
 @NgModule({
    declarations: [AppComponent],
@@ -16,9 +14,7 @@ import { AuthInterceptor } from '@core/interceptors/auth.interceptor';
       BrowserAnimationsModule,
       CoreModule
    ],
-   providers: [
-      { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
-   ],
+
    bootstrap: [AppComponent]
 })
 export class AppModule {}
--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -6,16 +6,15 @@ import { SharedModule } from '../shared/shared.module';
 import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './components/header/header.component';
 
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
+
 @NgModule({
    declarations: [HeaderComponent],
-   imports: [
-      CommonModule,
-      SharedModule,
-      HttpClientModule,
-      RouterModule
-   ],
-   exports: [
-      HeaderComponent
+   imports: [CommonModule, SharedModule, HttpClientModule, RouterModule],
+   exports: [HeaderComponent],
+   providers: [
+      { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
    ]
 })
-export class CoreModule { }
+export class CoreModule {}
