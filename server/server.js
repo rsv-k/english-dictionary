c925b06ede08bac8780febeb20d67342ba5fdb7b
rsv-k <rsv.kpast@gmail.com>
2020-06-26 15:40:26 +0300

    dotenv module removed

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 const entryPoint = require('./app');
@@ -12,5 +11,9 @@ app.listen(PORT, () => {
    console.log('server is running');
 });
 
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose
+   .connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+   })
    .then(() => console.log('mongo db is running'));
