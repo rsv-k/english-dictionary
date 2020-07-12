6df3b4858841bbd89c58e4e605ef5585318bfa48
rsv-k <rsv.kpast@gmail.com>
2020-07-12 10:29:05 +0300

    angularx-social-login initialized

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,15 +6,37 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from '@core/core.module';
 
+import {
+   SocialLoginModule,
+   SocialAuthServiceConfig
+} from 'angularx-social-login';
+import { GoogleLoginProvider } from 'angularx-social-login';
+
 @NgModule({
    declarations: [AppComponent],
    imports: [
       BrowserModule,
       AppRoutingModule,
       BrowserAnimationsModule,
-      CoreModule
+      CoreModule,
+      SocialLoginModule
    ],
-
+   providers: [
+      {
+         provide: 'SocialAuthServiceConfig',
+         useValue: {
+            autoLogin: false,
+            providers: [
+               {
+                  id: GoogleLoginProvider.PROVIDER_ID,
+                  provider: new GoogleLoginProvider(
+                     '489399788360-lgve4u69it8cru0r0f761gr64m1glpu3.apps.googleusercontent.com'
+                  )
+               }
+            ]
+         } as SocialAuthServiceConfig
+      }
+   ],
    bootstrap: [AppComponent]
 })
 export class AppModule {}
--- client/src/app/features/auth/pages/auth/auth.component.ts
+++ client/src/app/features/auth/pages/auth/auth.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
+import { SocialAuthService } from 'angularx-social-login';
+import { GoogleLoginProvider } from 'angularx-social-login';
+
 @Component({
    selector: 'app-auth',
    templateUrl: './auth.component.html',
@@ -8,12 +11,24 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class AuthComponent implements OnInit {
    form: string;
-   constructor(private route: ActivatedRoute, private router: Router) {}
+   constructor(
+      private route: ActivatedRoute,
+      private router: Router,
+      private socialAuthService: SocialAuthService
+   ) {}
 
    ngOnInit(): void {
       this.route.url.subscribe(data => {
          this.form = data[0].path;
       });
+
+      this.socialAuthService.authState.subscribe(user => {
+         console.log(user);
+      });
+   }
+
+   signInWithGoogle() {
+      this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID);
    }
 
    onNavigate() {
