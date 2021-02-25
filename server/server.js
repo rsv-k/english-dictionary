0c2c68e3ac7434f2b41146c01c9a827255567538
rsv_k <rsv.kpast@gmail.com>
2021-02-25 14:36:59 +0200

    add noSQL injection security

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const mongoSanitize = require('express-mongo-sanitize');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 const entryPoint = require('./app');
+
+// Sanitize data
+app.use(mongoSanitize());
 
 app.use(entryPoint);
 
@@ -12,5 +16,9 @@ app.listen(PORT, () => {
    console.log('server is running');
 });
 
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose
+   .connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+   })
    .then(() => console.log('mongo db is running'));
