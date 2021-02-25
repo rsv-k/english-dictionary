dbc91bd6878d690cf008e8eabcdcdf8029d94119
rsv_k <rsv.kpast@gmail.com>
2021-02-25 14:45:03 +0200

    add XSS protection and security headers

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
@@ -9,6 +11,12 @@ const entryPoint = require('./app');
 
 // Sanitize data
 app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
 
 app.use(entryPoint);
 
